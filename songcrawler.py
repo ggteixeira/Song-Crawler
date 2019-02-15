d493e7f21f8252ea1de1ebe4987c4f6a272790ed
guiemi <guiemi.teixeira@gmail.com>
2019-02-15 21:36:07 -0200

    Faz o scraper inicial raspar corretamente a lista de canções

---
+++ songcrawler.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def songcrawl(band_name):
+    page = requests.get(f"https://www.vagalume.com.br/{band_name}/")
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    song_list = soup.find_all(class_="nameMusic")
+
+    lista = list()
+    for song_name in song_list:
+        song = song_name.contents[0]
+        lista.append(song)
+        print(song)
+
+songcrawl("metallica")
