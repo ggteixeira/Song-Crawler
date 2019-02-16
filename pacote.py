bb2bfdec3bcf17c8fa54843f7ac476f2ad7691ab
guiemi <guiemi.teixeira@gmail.com>
2019-02-16 01:31:20 -0200

    Deixa o código pronto para receber mais de 1 parametro

--- pacote.py
+++ pacote.py
@@ -2,8 +2,9 @@ import sys
 from songcrawler import songcrawler
 
 def main(argv):
-    if len(argv) <= 2:
-        print(songcrawler(argv[0]))
-
+    if len(argv) == 1:
+        return songcrawler(argv[0])
+    elif len(argv) == 2:
+        return songcrawler(argv[0], argv[1])
 if __name__ == "__main__":
     main(sys.argv[1:])
