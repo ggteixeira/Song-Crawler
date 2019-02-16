e631d309d84bf3359fa163352f3459756edb58f9
guiemi <guiemi.teixeira@gmail.com>
2019-02-16 03:25:40 -0200

    Adiciona uma condição importante:
    Se o usuário não passar parametros, a função deverá printar a lista
    completa de canções

--- scraper.py
+++ scraper.py
@@ -12,8 +12,8 @@ def scraper(band_name, song_amount  = 0):
         songname = song_name.contents[0]
         songlist.append(songname)
 
-    # if song_amount == 0:
-    #     song_amount = len(songlist)
+    if song_amount == 0:
+        song_amount = len(songlist)
     
     for song in songlist[:song_amount]:
         print(song)
