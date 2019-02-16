cf61954c6c1552366b8e7925c15e07035689ad3d
guiemi <guiemi.teixeira@gmail.com>
2019-02-16 21:53:59 -0200

    Adiciona um map() ao código para transformar parametros da funcao na string esperada pela url

--- songcrawler.py
+++ songcrawler.py
@@ -1,10 +1,13 @@
 import sys
 from scraper import scraper
 
+
 def main(argv):
-    if len(argv) == 1:
-        return scraper(argv[0])
-    elif len(argv) == 2:
-        return scraper(argv[0], int(argv[1]))
+    # if len(argv) == 1:
+        return scraper(argv)
+    # elif len(argv) == 2:
+    #     return scraper(argv[0], (argv[1]))
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
