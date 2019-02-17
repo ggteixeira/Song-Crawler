0495048a2eaf6ed4a7caa3f4dfb3748166e6d371
guiemi <guiemi.teixeira@gmail.com>
2019-02-17 11:06:23 -0300

    Milestone: finaliza uma parte importante do crawler,
    que é proporcionar que qualquer nome de banda seja compreendido pela
    função. Outro milestone importante é implementar o print do número de
    canções pretendidas pelo usuário sem o uso do getopt ou do argparse.

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scraper(band_name, song_amount  = 0):
+
+def scraper(*args):
+    # Tratamento da string passada como parâmetro:
+    band_name = '-'.join(map(str, *args)).lower()
+
+
+    # Scraping:
     page = requests.get(f"https://www.vagalume.com.br/{band_name}/")
     soup = BeautifulSoup(page.text, 'html.parser')
-
     all_songs = soup.find_all(class_="nameMusic")
 
     songlist = list()
@@ -12,9 +17,21 @@ def scraper(band_name, song_amount  = 0):
         songname = song_name.contents[0]
         songlist.append(songname)
 
-    if song_amount == 0:
-        song_amount = len(songlist)
+    # Print da lista de canções
+    song_count = len(songlist)
+    print(f"O artista '{band_name}' possui {song_count} canções.")
     
-    for song in songlist[:song_amount]:
-        print(song)
-    print(f"Você pediu {song_amount} canções!")
+    prompt = True  # Condição de parada melhor que o break
+    while prompt:
+        num = int(input("Quantas canções você deseja mostrar? \n"))
+        if num > 0:
+            if num > song_count:
+                print("O número excede o total de canções. Tente novamente. \n")
+            else:
+                for song in songlist[:num]:
+                    print(song)
+                    prompt = False
+        else:
+            print("Você precisa selecionar o número de canções para mostrar. \n")
+
+    
--- songcrawler.py
+++ songcrawler.py
@@ -1,12 +1,10 @@
 import sys
+from argparse import ArgumentParser
 from scraper import scraper
 
 
 def main(argv):
-    # if len(argv) == 1:
-        return scraper(argv)
-    # elif len(argv) == 2:
-    #     return scraper(argv[0], (argv[1]))
+    return scraper(argv)
 
 
 if __name__ == "__main__":
