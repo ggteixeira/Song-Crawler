5504d664edd38d80cb717cd7402734ac02e6d0b7
guiemi <guiemi.teixeira@gmail.com>
2019-02-19 22:38:29 -0300

    Implementa o filtro de canções

--- songcrawler.py
+++ songcrawler.py
@@ -1,13 +1,27 @@
 import sys
 from scraper import songs_finder
 
+def filter_songs(songs_list):
+    prompt = True
+    print(f"O artista escolhido possui {len(songs_list)} canções. \n")
+    
+    while prompt:
+        num = int(input("Quantas canções você deseja mostrar? \n"))
+        if num > 0:
+            if num > len(songs_list):
+                print("O número excede o total de canções. Tente novamente.")
+            else:
+                filtered_list = map(lambda song: song, songs_list[:num]) 
+                prompt = False
+    
+    print('\n'.join(filtered_list))
 
 def main(*args):
     artist_name = ' '.join(map(str, *args))
     songs = songs_finder(artist_name)
-    print(f"{len(songs)} songs from {artist_name.title()}.")
-    print('\n'.join(songs))
-
+    # print(f"{len(songs)} songs from {artist_name.title()}.")
+    # print('\n'.join(songs))
+    return songs
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    filter_songs(main(sys.argv[1:]))
