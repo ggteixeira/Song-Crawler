6356ceccf0bbcfd64d50deb792893ed0a46ac5a1
guiemi <guiemi.teixeira@gmail.com>
2019-02-19 22:50:02 -0300

    WIP: control+option+command

--- songcrawler.py
+++ songcrawler.py
@@ -1,27 +1,30 @@
 import sys
 from scraper import songs_finder
 
+
 def filter_songs(songs_list):
     prompt = True
     print(f"O artista escolhido possui {len(songs_list)} canções. \n")
-    
+
     while prompt:
         num = int(input("Quantas canções você deseja mostrar? \n"))
         if num > 0:
             if num > len(songs_list):
                 print("O número excede o total de canções. Tente novamente.")
             else:
-                filtered_list = map(lambda song: song, songs_list[:num]) 
+                filtered_list = map(lambda song: song, songs_list[:num])
                 prompt = False
-    
+        else:
+            print("Você precisa selecionar o número de canções para mostrar. \n")
+
     print('\n'.join(filtered_list))
+
 
 def main(*args):
     artist_name = ' '.join(map(str, *args))
     songs = songs_finder(artist_name)
-    # print(f"{len(songs)} songs from {artist_name.title()}.")
-    # print('\n'.join(songs))
     return songs
+
 
 if __name__ == "__main__":
     filter_songs(main(sys.argv[1:]))
