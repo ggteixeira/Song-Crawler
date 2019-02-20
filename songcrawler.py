49f2208149af043f2675d1af97eccf3ba657f035
guiemi <guiemi.teixeira@gmail.com>
2019-02-20 20:31:19 -0300

    Simplifica as condições de print da lista de canções

--- songcrawler.py
+++ songcrawler.py
@@ -2,29 +2,28 @@ import sys
 from scraper import songs_finder
 
 
-def filter_songs(songs_list):
-    prompt = True
-    print(f"O artista escolhido possui {len(songs_list)} canções. \n")
+def display_songs(songs_list, size):
+    if size in range(1, len(songs_list)):
+        filtered_list = map(lambda song: song, songs_list[:size])
+    else:
+        filtered_list = map(lambda song: song, songs_list)
+    print("\n".join(filtered_list))
 
-    while prompt:
-        num = int(input("Quantas canções você deseja mostrar? \n"))
-        if num > 0:
-            if num > len(songs_list):
-                print("O número excede o total de canções. Tente novamente.")
-            else:
-                filtered_list = map(lambda song: song, songs_list[:num])
-                prompt = False
-        else:
-            print("Você precisa selecionar o número de canções para mostrar. \n")
 
-    print('\n'.join(filtered_list))
+def display_artist_info(artist_name, size_of_songs):
+    print(f"O artista {artist_name} possui {size_of_songs} canções. \n")
+
+
+def ask_for_size():
+    return int(input("Quantas canções você deseja mostrar? \n"))
 
 
 def main(*args):
     artist_name = ' '.join(map(str, *args))
     songs = songs_finder(artist_name)
-    return songs
+    display_artist_info(artist_name, len(songs))
+    display_songs(songs, ask_for_size())
 
 
 if __name__ == "__main__":
-    filter_songs(main(sys.argv[1:]))
+    main(sys.argv[1:])
